66a23f4c64844eaa1a6c2b67e35a288833d13049
Emeka Allison <emekaallison4@gmail.com>
2020-03-25 21:06:38 +0100

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotificationsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
